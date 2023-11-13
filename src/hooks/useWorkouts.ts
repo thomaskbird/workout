721620006baacb4d022b082dd1fa64be300de424
ThomasBird <thomasbird@ThomasBirds-MacBook-Pro.local>
2023-11-13 16:15:37 -0500

    fix: infinite loop inside of custom hook

--- src/hooks/useWorkouts.ts
+++ src/hooks/useWorkouts.ts
@@ -1,5 +1,7 @@
 import {
-  collectionWorkouts
+  collectionExercises,
+  collectionWorkouts,
+  firestoreDb,
 } from '@app/services/firebase';
 import { getUserWorkouts } from '@app/services/workouts';
 import { selectIsLoading, selectSetIsLoading } from '@app/store/selectors/globalStore';
@@ -8,8 +10,8 @@ import { useGlobalStore } from '@app/store/useGlobalStore';
 import { useSession } from '@app/store/useSession';
 import { WorkoutType } from '@app/types/types';
 import { makeArrayFromSnapshot } from '@app/utils/makeNewArray';
-import { QuerySnapshot, addDoc } from '@firebase/firestore';
-import { useEffect, useState } from 'react';
+import { QuerySnapshot, addDoc, doc, getDoc, getDocs, query, where } from '@firebase/firestore';
+import { useCallback, useEffect, useState } from 'react';
 
 const useWorkouts = (id?: string) => {
   const user = useSession(selectUser);
@@ -17,7 +19,32 @@ const useWorkouts = (id?: string) => {
   const isLoading = useGlobalStore(selectIsLoading);
   const setIsLoading = useGlobalStore(selectSetIsLoading);
 
+  const [workout, setWorkout] = useState<WorkoutType | undefined>(undefined);
   const [workouts, setWorkouts] = useState<WorkoutType[]>([]);
+
+  const retrieveWorkoutById = useCallback(async () => {
+    if(id) {
+      try {
+        console.log('try');
+        const workoutSnapshot = await getDoc(doc(firestoreDb, 'workouts', id));
+        const workoutData = workoutSnapshot.data();
+        const exerciseIds = workoutData.exercises.map(exercise => exercise.id);
+
+        const exercisesSnap = await getDocs(query(collectionExercises, where('id', 'in', exerciseIds)));
+        const exercisesData = makeArrayFromSnapshot(exercisesSnap);
+
+        setWorkout({
+          ...workoutData,
+          id: workoutSnapshot.id,
+          exercises: exercisesData
+        });
+      } catch (e) {
+        console.warn(e);
+      }
+    } else {
+      console.warn('No id available');
+    }
+  }, [id]);
 
   const retrieveAllWorkouts = async () => {
     setIsLoading(true);
@@ -48,8 +75,13 @@ const useWorkouts = (id?: string) => {
     retrieveAllWorkouts();
   }, []);
 
+  useEffect(() => {
+    retrieveWorkoutById();
+  }, [id]);
+
   return {
     isLoading,
+    workout,
     workouts,
     addWorkouts,
     retrieveAllWorkouts
--- src/pages/workouts/[workoutId].tsx
+++ src/pages/workouts/[workoutId].tsx
@@ -1,44 +1,16 @@
 import BackButton from '@app/components/BackButton/BackButton';
 import DisplayList from '@app/components/DisplayList/DisplayList';
 import ListItemExercise from '@app/components/ListItemExercise/ListItemExercise';
+import useWorkouts from '@app/hooks/useWorkouts';
 import styles from '@app/pages/exercises/index.module.scss';
-import { collectionExercises, firestoreDb } from '@app/services/firebase';
-import { makeArrayFromSnapshot } from '@app/utils/makeNewArray';
-import { doc, getDoc, getDocs, query, where } from '@firebase/firestore';
 import { Grid, Typography } from '@mui/material';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 
 const WorkoutView: NextPage = () => {
   const router = useRouter();
   const {workoutId} = router.query;
-  const [workout, setWorkout] = useState(null);
-
-  const retrieveWorkoutById = async () => {
-    try {
-      const workoutSnapshot = await getDoc(doc(firestoreDb, 'workouts', workoutId));
-      const workoutData = workoutSnapshot.data();
-      const exerciseIds = workoutData.exercises.map(exercise => exercise.id);
-
-      const exercisesSnap = await getDocs(query(collectionExercises, where('id', 'in', exerciseIds)));
-      const exercisesData = makeArrayFromSnapshot(exercisesSnap);
-
-      setWorkout({
-        ...workoutData,
-        id: workoutSnapshot.id,
-        exercises: exercisesData
-      });
-    } catch (e) {
-      console.warn(e);
-    }
-  }
-
-  useEffect(() => {
-    if(workoutId) {
-      retrieveWorkoutById();
-    }
-  }, [workoutId]);
+  const {workout} = useWorkouts(workoutId);
   
   return (
     <Grid container spacing={2} className={styles.exerciseWrapper}>
