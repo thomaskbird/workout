c80cdb4d950e8a5c85e91e3f27b5de4ee9df82ca
ThomasBird <thomasbird@ThomasBirds-MacBook-Pro.local>
2023-11-14 23:38:18 -0500

    chore: cleanup

--- src/pages/workouts/go/[workoutId].tsx
+++ src/pages/workouts/go/[workoutId].tsx
@@ -1,4 +1,6 @@
+import VideoPlayer from '@app/components/VideoPlayer/VideoPlayer';
 import useWorkouts from '@app/hooks/useWorkouts';
+import { ExerciseStepType } from '@app/types/types';
 import { Grid } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -10,52 +12,19 @@ import Stepper from '@mui/material/Stepper';
 import Typography from '@mui/material/Typography';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import * as React from 'react';
-
-const steps = [
-  {
-    label: 'Select campaign settings',
-    description: `For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.`,
-  },
-  {
-    label: 'Create an ad group',
-    description:
-      'An ad group contains one or more ads which target a shared set of keywords.',
-  },
-  {
-    label: 'Create an ad',
-    description: `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`,
-  },
-];
+import { useState } from 'react';
 
 const DoWorkout: NextPage = () => {
   const router = useRouter();
   const {workoutId} = router.query;
-  const {workout} = useWorkouts(workoutId);
+  const {workout} = useWorkouts(workoutId as string);
+
+  const [activeStep, setActiveStep] = useState(0);
 
   const exercises = workout?.exercises;
   const lastExerciseIndex = (exercises ?? []).length - 1;
 
   console.log(workout);
-  
-  const [activeStep, setActiveStep] = React.useState(0);
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
 
   return (
     <Grid container spacing={2}>
@@ -73,34 +42,43 @@ const DoWorkout: NextPage = () => {
                 >
                   {step.title}<br/> <Typography variant="caption">{step.description}</Typography>
                 </StepLabel>
+                
                 <StepContent>
                   <Grid container>
-                    <Grid md={9}>
-                      Video
+                    <Grid md={5}>
+                      <VideoPlayer
+                        url={step.video}
+                        thumbnail={step.thumbnail}
+                      />
                     </Grid>
-                    <Grid md={3}>
-                      Instructions
+                    <Grid md={7}>
+                      <ol>
+                      {(step.steps ?? []).map((exerciseStep: ExerciseStepType) => (
+                        <li key={exerciseStep.id}>{exerciseStep.val}</li>
+                      ))}
+                      </ol>
                     </Grid>
                   </Grid>
-                  <Typography>{step.description}</Typography>
+
                   <Box sx={{ mb: 2 }}>
                     <div>
                       <Button
                         variant="contained"
-                        onClick={handleNext}
+                        onClick={() => setActiveStep((prevActiveStep) => prevActiveStep + 1)}
                         sx={{ mt: 1, mr: 1 }}
                       >
                         {index === lastExerciseIndex ? 'Finish' : 'Continue'}
                       </Button>
                       <Button
                         disabled={index === 0}
-                        onClick={handleBack}
+                        onClick={() => setActiveStep((prevActiveStep) => prevActiveStep - 1)}
                         sx={{ mt: 1, mr: 1 }}
                       >
                         Back
                       </Button>
                     </div>
                   </Box>
+                  
                 </StepContent>
               </Step>
             ))}
@@ -108,7 +86,7 @@ const DoWorkout: NextPage = () => {
           {activeStep === (exercises ?? []).length && (
             <Paper square elevation={0} sx={{ p: 3 }}>
               <Typography>All steps completed - you&apos;re finished</Typography>
-              <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+              <Button onClick={() => setActiveStep(0)} sx={{ mt: 1, mr: 1 }}>
                 Reset
               </Button>
             </Paper>
