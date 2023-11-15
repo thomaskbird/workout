a62efbbfc444bffaa581bde413096d4a9a5b6e2c
ThomasBird <thomasbird@ThomasBirds-MacBook-Pro.local>
2023-11-15 02:39:42 -0500

    chore: styling

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,5 +1,6 @@
 import useCounter from "@app/hooks/useCounter";
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, Typography } from "@mui/material";
+import styles from './Counter.module.scss';
 
 const Counter = () => {
   const { current, start, pause, reset, isRunning } = useCounter(
@@ -9,18 +10,19 @@ const Counter = () => {
   );
   
   return (
-    <Grid>
-      <Typography>Counter</Typography>
-      <Typography>Current: {current}</Typography>
-
-      {isRunning ? (
-        <Button color="inherit" onClick={pause}>Pause</Button>
-      ): (
-        <Button color="primary" onClick={start} disabled={current === 0}>Start</Button>
-      )}
-    
-      <Button color="error" onClick={reset}>Reset</Button>
-    </Grid>
+    <Card>
+      <CardContent>
+        <Typography variant="h1" className={styles.main}>{current}</Typography>
+      </CardContent>
+      <CardActions>
+        {isRunning ? (
+          <Button color="inherit" onClick={pause}>Pause</Button>
+        ): (
+          <Button color="primary" onClick={start} disabled={current === 0}>Start</Button>
+        )}
+        <Button color="error" onClick={reset}>Reset</Button>
+      </CardActions>
+    </Card>
   )
 }
 
