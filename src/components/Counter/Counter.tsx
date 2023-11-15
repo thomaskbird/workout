8cc7002f624586d54763eab51fbad0daace8edd3
ThomasBird <thomasbird@ThomasBirds-MacBook-Pro.local>
2023-11-15 02:11:39 -0500

    chore: added a check to ensure time cant be called while current is 0

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -2,7 +2,7 @@ import useCounter from "@app/hooks/useCounter";
 import { Button, Grid, Typography } from "@mui/material";
 
 const Counter = () => {
-  const { current, start, pause, reset } = useCounter(
+  const { current, start, pause, reset, isRunning } = useCounter(
     5,
     () => console.log("started"),
     () => console.log("ended"),
@@ -13,8 +13,12 @@ const Counter = () => {
       <Typography>Counter</Typography>
       <Typography>Current: {current}</Typography>
 
-      <Button color="primary" onClick={start}>Start</Button>
-      <Button color="inherit" onClick={pause}>Pause</Button>
+      {isRunning ? (
+        <Button color="inherit" onClick={pause}>Pause</Button>
+      ): (
+        <Button color="primary" onClick={start}>Start</Button>
+      )}
+    
       <Button color="error" onClick={reset}>Reset</Button>
     </Grid>
   )
--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -6,37 +6,51 @@ const useCounter = (
   onEnd?: () => void,
 ) => {
   const intervalRef = useRef<undefined | NodeJS.Timeout>();
+
   const [current, setCurrent] = useState(end);
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
-    return () => clearInterval(intervalRef.current);
+    return () => {
+      clearInterval(intervalRef.current);
+      setIsRunning(false);
+    }
   }, []);
 
   const start = () => {
-    onStart && onStart();
-    const cycler = setInterval(() => {
-      setCurrent(prev => {
-        if((prev - 1) === 0) {
-          onEnd && onEnd();
-          clearInterval(intervalRef.current);
-          return 0;
-        } else {
-          return prev - 1;
-        }
-      })
-    }, 1000);
+    if(current !== 0) {
+      setIsRunning(true);
+      onStart && onStart();
+      const cycler = setInterval(() => {
+        setCurrent(prev => {
+          if((prev - 1) === 0) {
+            onEnd && onEnd();
+            setIsRunning(false);
+            clearInterval(intervalRef.current);
+            return 0;
+          } else {
+            return prev - 1;
+          }
+        })
+      }, 1000);
 
-    intervalRef.current = cycler;
+      intervalRef.current = cycler;
+    }
   };
 
-  const pause = () => clearInterval(intervalRef.current);
+  const pause = () => {
+    clearInterval(intervalRef.current);
+    setIsRunning(false);
+  }
 
   const reset = () => {
     setCurrent(end);
+    setIsRunning(false);
     clearInterval(intervalRef.current);
   }
 
   return {
+    isRunning,
     current,
     start,
     pause,
