b0f4f7d1bffcca150eff4ef3b7f123a6a330dfe8
Thomas Bird <thomasbird@Thomass-MacBook-Pro.local>
2023-12-17 22:00:35 -0500

    chore: adding better formatting for history display

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -4,11 +4,34 @@ import { useSession } from '@app/store/useSession';
 import { Divider, Grid, ListItemText, MenuItem, MenuList, Paper, Typography } from '@mui/material';
 import moment from 'moment';
 import { NextPage } from 'next';
+import {Timestamp} from "@firebase/firestore";
 
 const HistoryView: NextPage = () => {
   const user = useSession(selectUser);
   const history = user?.history;
 
+  const formatDate = (dateTimeStamp: Timestamp) => {
+    const jsTimestamp = new Date(dateTimeStamp.seconds * 1000 + dateTimeStamp.nanoseconds / 1000000);
+    return moment(jsTimestamp).format(config.momentFormat);
+  }
+
+  const formatDuration = (duration: string) => {
+    const durationParts = duration.split(':');
+    const hasHours = parseInt(durationParts[0], 10) > 0;
+    const hasMinutes = parseInt(durationParts[1], 10) > 0;
+
+    let formattedString = '';
+
+    if(hasHours) {
+      formattedString = `${durationParts[0]}hrs, ${durationParts[1]}mins and ${durationParts[0]}secs`;
+    } else if(hasMinutes) {
+      formattedString = `${durationParts[1]}mins and ${durationParts[0]}secs`;
+    } else {
+      formattedString = `${durationParts[2]}secs`;
+    }
+
+    return formattedString;
+  }
 
   return (
     <Grid container spacing={2}>
@@ -22,8 +45,9 @@ const HistoryView: NextPage = () => {
               <MenuItem key={item.id} component="div">
                 <ListItemText>{item?.workoutTitle}</ListItemText>
                 <Typography variant="body2" color="text.secondary">
-                  {moment(item.workoutDate).format(config.dateTimeFormat)}&nbsp;-&nbsp;
-                  {item.duration}
+                  <>
+                    <b>Completed:</b> {formatDate(item.workoutDate)}&nbsp;-&nbsp; <b>Duration:</b> {formatDuration(item.duration)}
+                  </>
                 </Typography>
                 <Divider />
               </MenuItem>
